6a1dd8a53bc307bf7f9128b8f0014ebf915d830f
Omar Lopez Garcia <omar.lopez@vallegrande.edu.pe>
2024-12-03 19:47:12 -0500

    Inital commit

--- src/services/apiService.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-async function fetchUserData(userId) {
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
-  return response.data;
-}
-
-module.exports = { fetchUserData };
--- tests/utils/math.test.js
+++
@@ -1,9 +0,0 @@
-const { add, subtract } = require('../../src/utils/math');
-
-test('adds two numbers', () => {
-  expect(add(2, 3)).toBe(5);
-});
-
-test('subtracts two numbers', () => {
-  expect(subtract(5, 3)).toBe(2);
-});
